940c91d01f7d0153d178df57edc6132923501f35
Alexandr <shura.tabolich@mail.ru>
2022-05-02 14:22:44 +0300

    feat: add fetching items from db

---
+++ src/actions/cartActions.js
@@ -0,0 +1,48 @@
+import {
+	CART_LIST_REQUEST,
+	CART_LIST_SUCCESS,
+	CART_LIST_FAIL,
+	CART_ITEM_ADD_FAIL,
+	CART_ITEM_ADD_REQUEST,
+	CART_ITEM_ADD_SUCCESS,
+	CART_ITEM_REMOVE_REQUEST,
+	CART_ITEM_REMOVE_SUCCESS,
+	CART_ITEM_REMOVE_FAIL,
+	CART_ITEM_UPDATE_REQUEST,
+	CART_ITEM_UPDATE_SUCCESS,
+	CART_ITEM_UPDATE_FAIL,
+} from "../constants/cartConstants";
+import db from "../firebase/config";
+import {
+	collection,
+	getDocs,
+	doc,
+	setDoc,
+	getDoc,
+	deleteDoc,
+	updateDoc,
+} from "firebase/firestore";
+
+export const listCartItems = () => async (dispatch) => {
+	let cartData = [];
+	async function getCartItems(db) {
+		const cartCol = collection(db, "cartItems");
+		const cartSnapshot = await getDocs(cartCol);
+		const cartList = cartSnapshot.docs.map((doc) => doc.data());
+		return cartList;
+	}
+
+	try {
+		dispatch({ type: CART_ITEM_UPDATE_REQUEST });
+		cartData = await getCartItems(db);
+		dispatch({ type: CART_ITEM_REMOVE_SUCCESS, payload: cartData });
+	} catch (error) {
+		dispatch({
+			type: CART_ITEM_UPDATE_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
--- src/containers/cart/CartScreen.js
+++ src/containers/cart/CartScreen.js
@@ -1,17 +1,20 @@
 import { CartContainerStyle } from "../../styles/CartScreen";
 import { CartItem } from "../../components/cart/CartItem";
 import Bounce from "react-reveal/Fade";
+import { useDispatch, useSelector } from "react-redux";
+import { listCartItems } from "../../actions/cartActions";
+import { useEffect } from "react";
 
 export const CartScreen = () => {
-	const cartItems = [
-		{
-			title: "Good Watch!",
-			price: 1999,
-			image:
-				"https://images.unsplash.com/photo-1551816230-ef5deaed4a26?ixlib=rb-1.2.1&",
-			qtyInCart: 1,
-		},
-	];
+	const dispatch = useDispatch();
+
+	const cartItemsList = useSelector((state) => state.cartItemsList);
+
+	const { loading, error, cartItems } = cartItemsList;
+
+	useEffect(() => {
+		dispatch(listCartItems());
+	}, [dispatch]);
 	return (
 		<div>
 			<h1>Cart</h1>
